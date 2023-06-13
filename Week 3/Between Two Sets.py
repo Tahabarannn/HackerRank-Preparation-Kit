8f05bc5589b24fe2ff40c9728862f5fd151ccff1
Tahabarannn <tahabaranakti10@gmail.com>
2023-06-13 13:41:07 +0300

    Week 3

---
+++ Week 3/Between Two Sets.py
@@ -0,0 +1,57 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import copy
+from functools import reduce
+
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def get_lcm(a, b):
+    return (a*b)//math.gcd(a, b)
+
+
+def getTotalX(a, b):
+    lcm = reduce(get_lcm, a)
+    gcd = reduce(math.gcd, b)
+
+    lcm_copy = copy.deepcopy(lcm)
+    counter = 0
+
+    while lcm_copy <= gcd:
+        if gcd % lcm_copy == 0:
+            counter += 1
+        lcm_copy += lcm
+
+    return counter
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
